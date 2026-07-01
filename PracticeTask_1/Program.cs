42ebec6778ce242e3513f0e9cb10b3e07c475b74
Ibtihaj <36j207@utas.edu.om>
2026-07-01 14:03:31 +0400

    Task 1 - Personal Info

---
+++ PracticeTask_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticeTask_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Personal Info Card
+            string name = "Ibtihaj";
+            int age = 24;
+            double height = 1.59;
+            bool isStudent = false;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Student: " + isStudent);
+
+
+        }
+    }
+}
