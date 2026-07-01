e349b698cc3723da6c71cfa07d3f5ea62acfd015
Ibtihaj <36j207@utas.edu.om>
2026-07-01 14:36:34 +0400

    Task 4 - Voting Eligibility

--- PracticeTask_1/Program.cs
+++ PracticeTask_1/Program.cs
@@ -19,31 +19,49 @@
 
 
             //Task 2 - Rectangle Calculator
-            Console.Write("Enter the length of the rectangle: ");
-            double length = double.Parse(Console.ReadLine());
+            //Console.Write("Enter the length of the rectangle: ");
+            //double length = double.Parse(Console.ReadLine());
 
-            Console.Write("Enter the width of the rectangle: ");
-            double width = double.Parse(Console.ReadLine());
+            //Console.Write("Enter the width of the rectangle: ");
+            //double width = double.Parse(Console.ReadLine());
 
-            double area = length * width;
-            double perimeter = 2 * (length + width);
+            //double area = length * width;
+            //double perimeter = 2 * (length + width);
 
-            Console.WriteLine("Area = " + area);
-            Console.WriteLine("Perimeter = " + perimeter);
+            //Console.WriteLine("Area = " + area);
+            //Console.WriteLine("Perimeter = " + perimeter);
 
             ///////////////////////////////////////////////////////////////////////////
 
             //Task 3 - Even or Odd Checker
-            Console.Write("Enter a number: ");
-            int number = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
 
-            if (number % 2 == 0)
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("The number is even.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number is odd.");
+            //}
+
+            ///////////////////////////////////////////////////////////////////////////
+
+            //Task 4 - Voting Eligibility
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Do you have a valid national ID? (yes/no): ");
+            string answer = Console.ReadLine();
+            
+            if (age >= 18 && answer == "yes")
             {
-                Console.WriteLine("The number is even.");
+                Console.WriteLine("You are eligible to vote");
             }
             else
             {
-                Console.WriteLine("The number is odd.");
+                Console.WriteLine("You are not eligible to vote");
             }
         }
 
