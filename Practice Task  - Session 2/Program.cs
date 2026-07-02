1629e7fb0767014d32d8e2b3d3a1ebe477575a8e
Ibtihaj <36j207@utas.edu.om>
2026-07-02 20:53:30 +0400

    Task 1 - Countdown Timer

---
+++ Practice Task  - Session 2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice_Task____Session_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // Task 1 - Countdown Timer
+            Console.Write("Enter the starting number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = number; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Liftoff!");
+        }
+    }
+}
